b3a6ee646a21c9fc14aa124276c26a96527f4375
Sage911 <stefan.age@outlook.com>
2017-05-08 21:33:26 -0500

    WIP - Registration form styles
    When SAVE is clicked. Registration form should reset.

--- src/components/registrationForm.js
+++ src/components/registrationForm.js
@@ -2,9 +2,17 @@
 // If form passes validation, we will push into the database
 
 import React from 'react'
-import { Control, Form, actions } from 'react-redux-form'
+import { Control, Form, actions, modelReducer } from 'react-redux-form'
 import { registerUser } from '../actions/registrationActions'
 import { connect } from 'react-redux'
+import styles from './RegistrationForm.css'
+
+const initialRegisterState = {
+  name: '',
+  experience: '',
+  specialty: '',
+  email: ''
+}
 
 @connect((store) => { return {} })
 
@@ -12,7 +20,7 @@ class RegistrationForm extends React.Component {
 
   handleSubmit(values) {
     // this.props.dispatch(registerUser(values))
-
+    this.props.dispatch(actions.setInitial('registration'))
   }
 
   isEmail(userEmail) {
@@ -22,14 +30,17 @@ class RegistrationForm extends React.Component {
   render() {
 
     return (
-      <Form name="registration" model="forms.registration" onSubmit={ values => this.handleSubmit(values)}>
-        <Control.text model="registration.name" maxLength="25" placeholder="Name" required />
-        <Control.text type="number" max="99" model="registration.experience" placeholder="Experience" required />
-        <Control.text model="registration.specialty" placeholder="Specialty" required/>
-        {/* change to drop-down */}
-        <Control.text type="email" model="registration.email" placeholder="Email" required />
-        <button type="submit">Submit</button>
-      </Form>
+        <main className={styles.main}>
+          <Form className={styles.registrationForm} name="registration" model="forms.registration" onSubmit={ values => this.handleSubmit(values)}>
+            <h1 className={styles.registrationForm__label}>ABOUT YOU</h1>
+          <Control.text className={styles.registrationForm__input} model="registration.name" maxLength="25" placeholder="Name" required />
+          <Control.text className={styles.registrationForm__input} type="number" max="99" model="registration.experience" placeholder="Experience" required />
+          <Control.text className={styles.registrationForm__input} model="registration.specialty" placeholder="Specialty" required/>
+          {/* change to drop-down */}
+          <Control.text className={styles.registrationForm__input} type="email" model="registration.email" placeholder="Email" required />
+          <span className={styles.registrationForm__saveSection}><button className={styles.registrationForm__save} type="submit">SAVE</button></span>
+        </Form>
+      </main>
     )
   }
 }
